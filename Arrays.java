51f07608e21321b415219d76f01bb0aaecd592d9
Steven Gerdes <stevenagerdes@gmail.com>
2016-11-11 11:41:25 -0600

    Moved CodingBat Problems to New Folder

---
+++ Arrays.java
@@ -0,0 +1,28 @@
+public class Arrays {
+
+/*
+Given an array of ints, return true if 6 appears as either the first
+or last element in the array. The array will be length 1 or more.*/
+    public boolean firstLast6(int[] nums) {
+        if (nums[0] == 6 || nums[nums.length -1] == 6) {
+            return true;
+        }
+        return false;
+    }
+/*Given an array of ints, return true if the array is length 1 or more, and the first
+element and the last element are equal.*/
+    public boolean sameFirstLast(int[] nums) {
+
+        if (nums.length >= 1 && nums[0] == nums[nums.length - 1]) {
+            return true;
+        }
+        return false;
+    }
+
+/*Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.*/
+    public int[] makePi() {
+        int[] piArray = new int[] {3, 1, 4};
+        return piArray;
+    }
+
+}
