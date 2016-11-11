6a05aaf491b1877c3d6d2b82948faecefe46adaf
Steven Gerdes <stevenagerdes@gmail.com>
2016-11-11 14:23:54 -0600

    New Arrays -- Loop

--- Arrays.java
+++ Arrays.java
@@ -1,7 +1,6 @@
 public class Arrays {
 
-/*
-Given an array of ints, return true if 6 appears as either the first
+/*Given an array of ints, return true if 6 appears as either the first
 or last element in the array. The array will be length 1 or more.*/
     public boolean firstLast6(int[] nums) {
         if (nums[0] == 6 || nums[nums.length -1] == 6) {
@@ -23,6 +22,58 @@ element and the last element are equal.*/
     public int[] makePi() {
         int[] piArray = new int[] {3, 1, 4};
         return piArray;
+    }
+
+/*Given 2 arrays of ints, a and b, return true if they have the same first element or
+they have the same last element. Both arrays will be length 1 or more.*/
+    public boolean commonEnd(int[] a, int[] b) {
+        if ((a[0] == b[0]) || (a[a.length -1] == b[b.length -1])) {
+            return true;
+        }
+        return false;
+    }
+
+/*Given an array of ints length 3, return the sum of all the elements.*/
+    public int sum3(int[] nums) {
+        int sum = 0;
+
+        for (int i : nums) {
+            sum += i;
+        }
+        return sum;
+    }
+
+/*Given an array of ints length 3, return an array with the elements "rotated left"
+so {1, 2, 3} yields {2, 3, 1}.*/
+    public int[] rotateLeft3(int[] nums) {
+        int willBe0 = nums[1];
+        int willBe1 = nums[2];
+        int willBe2 = nums[0];
+
+        int[] newArray = new int[]{willBe0, willBe1, willBe2};
+        return newArray;
+    }
+
+//The for loop would be better if the size is longer or unknown:
+    public int[] rotateLeft3(int[] nums) {
+        int temp = nums[0];
+        for (int i = 0; i < 2; i++) {
+            nums[i] = nums[i + 1];
+        }
+        nums[2] = temp;
+
+        return nums;
+    }
+
+/*Given an array of ints length 3, return a new array with the elements in reverse
+order, so {1, 2, 3} becomes {3, 2, 1}.*/
+    public int[] reverse3(int[] nums) {
+        int first = nums[2];
+        int last = nums[0];
+        int middle = nums[1];
+        int[] newArray = new int[] {first, middle, last};
+        return newArray;
+//a good for loop for this one???
     }
 
 }
--- Warmup1.java
+++ Warmup1.java
@@ -1,6 +1,5 @@
-package LearningStrings;
-
 public class Warmup1 {
+
 /*We'll say that a number is "teen" if it is in the range 13..19 inclusive.
 Given 2 int values, return true if one or the other is teen, but not both.*/
     public boolean loneTeen(int a, int b) {
