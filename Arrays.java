3e8c1b36040ed1d2e7f1051f8550cb2948d97c74
Steven Gerdes <stevenagerdes@gmail.com>
2016-11-11 21:05:49 -0600

    Finished Arrays1

--- Arrays.java
+++ Arrays.java
@@ -42,6 +42,10 @@ they have the same last element. Both arrays will be length 1 or more.*/
         }
         return sum;
     }
+//OR:
+    public int sum3(int[] nums) {
+        return nums[0] + nums[1] + nums[2];
+    }
 
 /*Given an array of ints length 3, return an array with the elements "rotated left"
 so {1, 2, 3} yields {2, 3, 1}.*/
@@ -109,5 +113,237 @@ elements from the original array. The original array will be length 1 or more.*/
         return new int[] {nums[0], nums[nums.length - 1]};
     }
 
+/*Given an int array length 2, return true if it contains a 2 or a 3.*/
+    public boolean has23(int[] nums) {
+        for (int i = 0; i <= nums.length - 1; i++) {
+            if (nums[i] == 2 || nums[i] == 3) {
+                return true;
+            }
+        }
+        return false;
+    }
 
+/*Given an int array length 2, return true if it does not contain a 2 or 3.*/
+/*Tried reversing the operand to != and keeping the method the same but does not work.
+Some tests still come up true. Switch the return values so that if 2 or 3 show up
+in either loop passes a false value is returned.*/
+    public boolean no23(int[] nums) {
+        for (int i = 0; i <= nums.length -1; i++) {
+            if (nums[i] == 2 || nums[i] == 3) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+/*
+Given an int array, return a new array with double the length where its last
+element is the same as the original array, and all the other elements are 0.
+The original array will be length 1 or more. Note: by default, a new int array
+contains all 0's.*/
+    public int[] makeLast(int[] nums) {
+        int[] doubleTheLength = new int[nums.length * 2];
+        doubleTheLength[doubleTheLength.length - 1] = nums[nums.length - 1];
+        return doubleTheLength;
+    }
+
+/*Given an int array, return true if the array contains 2 twice, or 3 twice.
+The array will be length 0, 1, or 2.*/
+    public boolean double23(int[] nums) {
+        if (nums.length > 1 && nums[0] == 2 && nums[1] == 2) {
+            return true;
+        } else if (nums.length > 1 && nums[0] == 3 && nums[1] == 3) {
+            return true;
+        }
+        return false;
+    }
+//Better to use a for loop?
+    public boolean double23(int[] nums) {
+        int count2 = 0;
+        int count3 = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 2) {
+                count2++;
+            }
+            if (nums[i] == 3) {
+                count3++;
+            }
+        }
+        return count2 == 2 || count3 == 2;
+    }
+
+/*
+Given an int array length 3, if there is a 2 in the array
+immediately followed by a 3, set the 3 element to 0. Return the changed array.*/
+    public int[] fix23(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 2 && nums[i + 1] == 3) {
+                nums[i + 1] = 0;
+            }
+        }
+        return nums;
+    }
+
+/*Start with 2 int arrays, a and b, of any length. Return how many of the arrays
+have 1 as their first element.*/
+    public int start1(int[] a, int[] b) {
+        int count = 0;
+        for (int i = 0; i < 1; i++) {
+            if (a.length > 0 && a[0] == 1) {
+                count++;
+            }
+            if (b.length > 0 && b[0] == 1) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+/*Start with 2 int arrays, a and b, each length 2. Consider the sum of the values
+in each array. Return the array which has the largest sum. In event of a tie, return a.*/
+    public int[] biggerTwo(int[] a, int[] b) {
+
+        if (b[0] + b[1] > a[0] + a[1]) {
+            return b;
+        }
+        return a;
+    }
+
+/*Given an array of ints of even length, return a new array length 2 containing the middle
+two elements from the original array. The original array will be length 2 or more.*/
+//Local variables aren't necessary -- plug length math into return statement.
+    public int[] makeMiddle(int[] nums) {
+        int middleLeft = nums.length / 2 - 1;
+        int middleRight = nums.length / 2;
+        return new int[] {nums[middleLeft], nums[middleRight]};
+    }
+
+/*Given 2 int arrays, each length 2, return a new array length 4 containing all
+their elements.*/
+    public int[] plusTwo(int[] a, int[] b) {
+        return new int[] {a[0], a[1], b[0], b[1]};
+    }
+
+/*Given an array of ints, swap the first and last elements in the array. Return the
+modified array. The array length will be at least 1.*/
+    public int[] swapEnds(int[] nums) {
+        int save0 = nums[0];
+        nums[0] = nums[nums.length -1];
+        nums[nums.length -1] = save0;
+        return nums;
+    }
+
+/*Given an array of ints of odd length, return a new array length 3 containing the elements
+from the middle of the array. The array length will be at least 3.*/
+    public int[] midThree(int[] nums) {
+        int[] middle3 = new int[3];
+        middle3[0] =  nums[nums.length / 2 - 1];
+        middle3[1] = nums[nums.length / 2];
+        middle3[2] = nums[nums.length / 2 + 1];
+
+        return middle3;
+    }
+//Faster but not as readable:
+    public int[] midThree(int[] nums) {
+        int middle = nums.length / 2;
+        int middleLeft = middle -1;
+        int middleRight = middle +1;
+
+        return new int[] {nums[middleLeft], nums[middle], nums[middleRight]};
+    }
+
+/*
+Given an array of ints of odd length, look at the first, last, and middle values in the
+array and return the largest. The array length will be a least 1.*/
+    public int maxTriple(int[] nums) {
+        int max = Math.max(Math.max(nums[0], nums[nums.length / 2]), nums[nums.length -1]);
+        return max;
+    }
+
+/*
+Given an int array of any length, return a new array of its first 2 elements. If the array
+is smaller than length 2, use whatever elements are present.*/
+    public int[] frontPiece(int[] nums) {
+        if (nums.length == 0) {
+            return nums;
+        } else if (nums.length == 1) {
+            return new int[] {nums[0]};
+        }
+        return new int[] {nums[0], nums[1]};
+    }
+
+/*We'll say that a 1 immediately followed by a 3 in an array is an "unlucky" 1. Return true
+if the given array contains an unlucky 1 in the first 2 or last 2 positions in the array.*/
+    public boolean unlucky1(int[] nums) {
+        if(nums.length >= 2 &&
+                (nums[0] == 1 && nums[1] == 3 || nums[nums.length - 2] == 1 &&
+                        nums[nums.length - 1] == 3))
+            return true;
+
+        if(nums.length >= 3 && nums[1] == 1 && nums[2] == 3)
+            return true;
+
+        return false;
+    }
+
+/*
+Given 2 int arrays, a and b, return a new array length 2 containing, as much as will
+fit, the elements from a followed by the elements from b. The arrays may be any length, including 0,
+but there will be 2 or more elements available between the 2 arrays.*/
+    public int[] make2(int[] a, int[] b) {
+        int[] only2 = new int[2];
+
+        if (a.length < 1) {
+            only2[0] = b[0];
+            only2[1] = b[1];
+        }
+        if (a.length == 1) {
+            only2[0] = a[0];
+            only2[1] = b[0];
+        }
+        if (a.length >= 2) {
+            only2[0] = a[0];
+            only2[1] = a[1];
+        }
+        return only2;
+    }
+//OR use 2 while loops:
+    public int[] make2(int[] a, int[] b) {
+        int[] arr = new int[2];
+        int count = 0;
+        int i;
+
+        i = 0;
+        while (count < 2 && i < a.length) {
+            arr[count] = a[i];
+            count++;
+            i++;
+        }
+
+        i = 0;
+        while(count < 2 && i < b.length) {
+            arr[count] = b[i];
+            count++;
+            i++;
+        }
+        return arr;
+    }
+
+/*
+Given 2 int arrays, a and b, of any length,
+return a new array with the first element of each array. If either array is length 0, ignore that array.*/
+    public int[] front11(int[] a, int[] b) {
+        if (a.length == 0 && b.length == 0) {
+            return a;
+        }
+        if (a.length == 0 && b.length > 0) {
+            return new int[] {b[0]};
+        }
+        if (a.length > 0 && b.length == 0) {
+            return new int[] {a[0]};
+        }
+        return new int[] {a[0], b[0]};
+    }
 }
+
