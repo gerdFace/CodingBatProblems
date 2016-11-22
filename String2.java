59bf35f564ad0e6ae502ad09075cb32398b690aa
gerdface <stevenagerdes@gmail.com>
2016-11-21 22:40:25 -0600

    String2 nearly complete

--- String2.java
+++ String2.java
@@ -204,4 +204,21 @@ public class String2 {
         return emptyString;
     }
 
+    /*Look for patterns like "zip" and "zap" in the string -- length-3, starting with 'z' and ending with 'p'.
+    Return a string where for all such words, the middle letter is gone, so "zipXzap" yields "zpXzp".*/
+    public String zipZap(String str) {
+        String newString = "";
+        if (str.length() < 3) return str;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'z' && str.charAt(i +2) == 'p') {
+                newString += str.substring(i, i +1) + str.substring(i +2, i +3);
+                i += 2;
+            } else {
+                newString += str.substring(i, i +1);
+            }
+        }
+        return newString;
+    }
+    //The same can be achieved by using one line of code:
+    return str.replaceAlll("z.p", "zp");
 }
