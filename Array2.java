f52759c3afe25309c2826fdd84405930793e6730
Steven Gerdes <stevenagerdes@gmail.com>
2016-12-22 14:24:09 -0600

    Arrays 2

--- Array2.java
+++ Array2.java
@@ -23,4 +23,38 @@ public class Array2 {
         return maxNum - minNum;
     }
 
+    /*Return the "centered" average of an array of ints, which we'll say is the mean average of the values, except
+    ignoring the largest and smallest values in the array. If there are multiple copies of the smallest value, ignore
+    just one copy, and likewise for the largest value. Use int division to produce the final average.
+    You may assume that the array is length 3 or more.*/
+    public int centeredAverage(int[] nums) {
+        int totalNum = 0;
+        int maxNum = nums[0];
+        int minNum = nums[0];
+
+        for (int i:nums) {
+            maxNum = Math.max(i, maxNum);
+            minNum = Math.min(i, minNum);
+            totalNum += i;
+        }
+        return (totalNum - (maxNum + minNum)) / (nums.length -2);
+    }
+
+    /*Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 13 is very
+    unlucky, so it does not count and numbers that come immediately after a 13 also do not count.*/
+    public int sum13(int[] nums) {
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 13 && i != nums.length - 1) {
+                nums[i + 1] = 0;
+            }
+            if (nums[i] == 13) {
+                nums[i] = 0;
+            }
+            sum += nums[i];
+        }
+        return sum;
+    }
+
 }
