f3ceabd35dba65d5fa3da5658e47504a2255448b
Steven Gerdes <stevenagerdes@gmail.com>
2016-12-22 16:50:52 -0600

    More Logic1

--- Logic1.java
+++ Logic1.java
@@ -37,4 +37,89 @@ public class Logic1 {
         return false;
     }
 
+    /*You are driving a little too fast, and a police officer stops you. Write code to compute the result, encoded as
+    an int value: 0=no ticket, 1=small ticket, 2=big ticket. If speed is 60 or less, the result is 0. If speed
+    is between 61 and 80 inclusive, the result is 1. If speed is 81 or more, the result is 2. Unless it is
+    your birthday -- on that day, your speed can be 5 higher in all cases.*/
+    public int caughtSpeeding(int speed, boolean isBirthday) {
+        int result = 0;
+
+        if ((!isBirthday && (speed > 60 && speed <= 80)) || (isBirthday && (speed > 65 && speed <= 85))) {
+            result = 1;
+        }
+
+        if ((!isBirthday && speed > 80) || (isBirthday && speed > 85)) {
+            result = 2;
+        }
+        return result;
+    }
+
+    /*Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that
+    case just return 20.*/
+    public int sortaSum(int a, int b) {
+        int sum = a + b;
+
+        if (sum >= 10 && sum <= 19) {
+            sum = 20;
+        }
+        return sum;
+    }
+
+    /*Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat, and a boolean indicating if we are on vacation,
+    return a string of the form "7:00" indicating when the alarm clock should ring. Weekdays, the alarm should be "7:00"
+    and on the weekend it should be "10:00". Unless we are on vacation -- then on weekdays it should be "10:00" and
+    weekends it should be "off".*/
+    public String alarmClock(int day, boolean vacation) {
+        String alarm = "7:00";
+
+        if (((vacation) && (day >= 1 && day <= 5)) || ((!vacation) && (day == 0 || day == 6))) {
+            alarm = "10:00";
+        }
+
+        if ((vacation) && (day == 0 || day == 6)) {
+            alarm = "off";
+        }
+        return alarm;
+    }
+
+    /*The number 6 is a truly great number. Given two int values, a and b, return true if either one is 6. Or if their sum
+    or difference is 6. Note: the function Math.abs(num) computes the absolute value of a number.*/
+    public boolean love6(int a, int b) {
+        boolean hasSix = false;
+
+        if (a == 6 || b == 6) {
+            hasSix = true;
+        }
+        if ((a + b == 6) || (Math.abs(a - b) == 6)) {
+            hasSix = true;
+        }
+        return hasSix;
+    }
+
+    /*Given a number n, return true if n is in the range 1..10, inclusive. Unless "outsideMode" is true, in which case return
+    true if the number is less or equal to 1, or greater or equal to 10.*/
+    public boolean in1To10(int n, boolean outsideMode) {
+        boolean inRange = false;
+
+        if ((!outsideMode) && (n >= 1 && n <= 10)) {
+            inRange = true;
+        }
+        if ((outsideMode) && (n <= 1 || n >= 10)) {
+            inRange = true;
+        }
+        return inRange;
+    }
+
+    /*We'll say a number is special if it is a multiple of 11 or if it is one more than a multiple of 11. Return
+    true if the given non-negative number is special. Use the % "mod" operator -- see Introduction to Mod*/
+    public boolean specialEleven(int n) {
+        boolean isSpecial = false;
+
+        if (n%11 == 0 || n%11 == 1) {
+            isSpecial = true;
+        }
+        return isSpecial;
+    }
+
 }
+
