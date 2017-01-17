30f1150bca5f0990b1d8dbaf5df7659a028c6494
Steven Gerdes <stevenagerdes@gmail.com>
2017-01-17 13:12:31 -0600

    Finished Logic1 almost done with Array2

--- Array2.java
+++ Array2.java
@@ -361,4 +361,25 @@ public class Array2 {
         return newFizzArray;
     }
 
+    /*Given a non-empty array of ints, return a new array containing the elements from the original array that come
+    before the first 4 in the original array. The original array will contain at least one 4. Note that it is valid
+    in java to create an array of length 0.*/
+    public int[] pre4(int[] nums) {
+        int arraySize = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 4) {
+                break;
+            }
+            arraySize++;
+        }
+
+        int[] no4Array = new int[arraySize];
+
+        for (int i = 0; i < no4Array.length; i++) {
+            no4Array[i] = nums[i];
+        }
+        return no4Array;
+    }
+
 }
