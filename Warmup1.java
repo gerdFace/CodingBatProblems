dd367afd6512a92e1d5f9caf2491aa91524a0510
gerdface <stevenagerdes@gmail.com>
2017-02-20 22:08:25 -0600

    Finishing Warmup1

--- Warmup1.java
+++ Warmup1.java
@@ -58,4 +58,17 @@ string where that "del" has been deleted. Otherwise, return the string unchanged
         }
     }
 
+    /*Given 2 int values, return true if they are both in the range 30..40 inclusive, or they are both in the range
+    40..50 inclusive.*/
+    public boolean in3050(int a, int b) {
+        if ((a >= 30 && a <= 40) && (b >= 30 && b <= 40)) {
+            return true;
+        }
+        if ((a >= 40 && a <= 50) && (b >= 40 && b <= 50)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
