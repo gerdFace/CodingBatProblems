a46dff49876136ce35e10465398f1fa71255614f
gerdface <stevenagerdes@gmail.com>
2017-02-22 01:39:04 -0600

    Finishing Warmup1

--- Warmup1.java
+++ Warmup1.java
@@ -71,4 +71,59 @@ string where that "del" has been deleted. Otherwise, return the string unchanged
         }
     }
 
+    /*Given 2 positive int values, return the larger value that is in the range 10..20 inclusive, or return 0 if
+    neither is in that range.*/
+    public int max1020(int a, int b) {
+        int maxValue = Math.max(a, b);
+        int minValue = Math.min(a, b);
+
+        if (maxValue >= 10 && maxValue <= 20) {
+            return maxValue;
+        }
+        if (minValue >= 10 && minValue <= 20) {
+            return minValue;
+        } else {
+            return 0;
+        }
+    }
+
+    /*Return true if the given string contains between 1 and 3 'e' chars.*/
+    public boolean stringE(String str) {
+        int eCount = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'e') {
+                eCount++;
+            }
+        }
+        return (eCount >= 1 && eCount <= 3);
+    }
+
+    /*Given two non-negative int values, return true if they have the same last digit, such as with 27 and 57.
+    Note that the % "mod" operator computes remainders, so 17 % 10 is 7.*/
+    public boolean lastDigit(int a, int b) {
+        return (a % 10 == b % 10);
+    }
+
+    /*Given a string, return a new string where the last 3 chars are now in upper case. If the string has less than 3
+    chars, uppercase whatever is there. Note that str.toUpperCase() returns the uppercase version of a string.*/
+    public String endUp(String str) {
+        if (str.length() > 3) {
+            String temp = str.substring(str.length() - 3, str.length()).toUpperCase();
+            return str.substring(0, str.length() - 3) + temp;
+        } else {
+            return str.toUpperCase();
+        }
+    }
+
+    /*Given a non-empty string and an int N, return the string made starting with char 0, and then every Nth char of
+    the string. So if N is 3, use char 0, 3, 6, ... and so on. N is 1 or more.*/
+    public String everyNth(String str, int n) {
+        String stringNth = "";
+        for (int i = 0; i < str.length(); i = i + n) {
+            stringNth += str.charAt(i);
+        }
+        return stringNth;
+    }
+
 }
