4f6f781b21358bd07cfa8502fcd054f9277b1318
gerdface <stevenagerdes@gmail.com>
2017-03-06 16:03:14 -0600

    Finished 8 warmup1 problems

--- Warmup1.java
+++ Warmup1.java
@@ -1,7 +1,74 @@
 public class Warmup1 {
 
-/*CodingBat problem --must return first and second character if they
-* are 'o' or 'z', respectively.*/
+    /*Return true if the given non-negative number is a multiple of 3 or a multiple of 5. Use the % "mod"
+    operator -- see Introduction to Mod*/
+    public boolean or35(int n) {
+        return (n % 3 == 0 || n % 5 == 0);
+    }
+
+    /*Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back,
+    so "kitten" yields"kikittenki". If the string length is less than 2, use whatever chars are there.*/
+    public String front22(String str) {
+        if (str.length() < 2) {
+            return str.concat(str).concat(str);
+        } else {
+            String first2Chars = str.substring(0, 2);
+            return first2Chars.concat(str).concat(first2Chars);
+        }
+    }
+
+    /*Given a string, return true if the string starts with "hi" and false otherwise.*/
+    public boolean startHi(String str) {
+        return str.startsWith("hi");
+    }
+
+    /*Given two temperatures, return true if one is less than 0 and the other is greater than 100.*/
+    public boolean icyHot(int temp1, int temp2) {
+        return (temp1 < 0 && temp2 > 100 || temp1 >100 && temp2 < 0);
+    }
+
+    /*Given 2 int values, return true if either of them is in the range 10..20 inclusive.*/
+    public boolean in1020(int a, int b) {
+        return (a >= 10 && a <= 20 || b >= 10 && b <= 20);
+    }
+
+    /*We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given 3 int values, return true if 1 or more of them are teen.*/
+    public boolean hasTeen(int a, int b, int c) {
+        return (a >= 13 && a <= 19 || b >= 13 && b <= 19 || c >= 13 && c <= 19);
+    }
+
+    /*We'll say that a number is "teen" if it is in the range 13..19 inclusive.
+    Given 2 int values, return true if one or the other is teen, but not both.*/
+    public boolean loneTeen(int a, int b) {
+        boolean isTeenA = a >= 13 && a <= 19;
+        boolean isTeenB = b >= 13 && b <= 19;
+        return (isTeenA || isTeenB) && !(isTeenA && isTeenB);
+    }
+
+    /*Given a string, if the string "del" appears starting at index 1, return a
+    string where that "del" has been deleted. Otherwise, return the string unchanged.*/
+    public String delDel(String str) {
+        if (str.length() <= 3) {
+            return str;
+        }
+        String findDel = str.substring(1, 4);
+        if (findDel.equals("del")) {
+            return str.substring(0, 1).concat(str.substring(4));
+        }
+        return str;
+    }
+
+    /*Return true if the given string begins with "mix", except the 'm' can be anything, so "pix", "9ix" .. all count.*/
+    public boolean mixStart(String str) {
+        if (str.length() > 2) {
+            return str.substring(1, 3).equals("ix");
+        } else {
+            return false;
+        }
+    }
+
+
+    /*CodingBat problem --must return first and second character if they are 'o' or 'z', respectively.*/
     public String startOz(String str) {
         String result = "";
 
@@ -13,27 +80,6 @@ public class Warmup1 {
             result = result + str.charAt(1);
         }
         return result;
-    }
-
-/*We'll say that a number is "teen" if it is in the range 13..19 inclusive.
-Given 2 int values, return true if one or the other is teen, but not both.*/
-    public boolean loneTeen(int a, int b) {
-        boolean isTeenA = a >= 13 && a <= 19;
-        boolean isTeenB = b >= 13 && b <= 19;
-        return (isTeenA || isTeenB) && !(isTeenA && isTeenB);
-    }
-
-/*Given a string, if the string "del" appears starting at index 1, return a
-string where that "del" has been deleted. Otherwise, return the string unchanged.*/
-    public String delDel(String str) {
-        if (str.length() <= 3) {
-            return str;
-        }
-        String findDel = str.substring(1, 4);
-        if (findDel.equals("del")) {
-            return str.substring(0, 1).concat(str.substring(4));
-        }
-        return str;
     }
 
     /*Given three int values, a b c, return the largest.*/
